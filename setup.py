3a1d426fdacb6fd39dc15ee59d72d48ffc78cb7b
lucarebuffi <lrebuffi@anl.gov>
2019-03-27 15:08:24 -0500

    First Commit First Commit

---
+++ setup.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python3
+
+import imp
+import os
+import subprocess
+
+try:
+    from setuptools import find_packages, setup
+except AttributeError:
+    from setuptools import find_packages, setup
+
+NAME = 'wavepytools'
+
+VERSION = '1.0.0'
+ISRELEASED = True
+
+DESCRIPTION = 'Wavepy Tools'
+README_FILE = os.path.join(os.path.dirname(__file__), 'README.rst')
+LONG_DESCRIPTION = open(README_FILE).read()
+AUTHOR = 'Walan Grizolli'
+AUTHOR_EMAIL = 'wcgrizolli@anl.gov'
+URL = 'https://github.com/aps-xsd-opt-group/wavepytools'
+DOWNLOAD_URL = 'https://github.com/aps-xsd-opt-group/wavepytools'
+MAINTAINER = 'XSD-OPT Group @ APS-ANL'
+MAINTAINER_EMAIL = 'lrebuffi@anl.gov'
+LICENSE = 'BSD-3'
+
+KEYWORDS = (
+    'dictionary',
+    'glossary',
+    'synchrotron'
+    'simulation',
+)
+
+CLASSIFIERS = (
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Environment :: Console',
+    'Environment :: Plugins',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Intended Audience :: Science/Research',
+)
+
+INSTALL_REQUIRES = (
+    'wavepy',
+)
+
+SETUP_REQUIRES = (
+    'setuptools',
+)
+
+
+# Return the git revision as a string
+def git_version():
+    """Return the git revision as a string.
+
+    Copied from numpy setup.py
+    """
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
+def write_version_py(filename='wavepytools/version.py'):
+    # Copied from numpy setup.py
+    cnt = """
+# THIS FILE IS GENERATED FROM wavepytools SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+
+if not release:
+    version = full_version
+    short_version += ".dev"
+"""
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('wavepytools/version.py'):
+        # must be a source distribution, use existing version file
+        version = imp.load_source("wavepytools.version", "wavepytools/version.py")
+        GIT_REVISION = version.git_revision
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
+
+PACKAGE_DATA = {
+}
+
+
+def setup_package():
+    write_version_py()
+    setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        license=LICENSE,
+        keywords=KEYWORDS,
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        # extra setuptools args
+        zip_safe=False,  # the package can run out of an .egg file
+        include_package_data=True,
+        install_requires=INSTALL_REQUIRES,
+        setup_requires=SETUP_REQUIRES,
+    )
+
+if __name__ == '__main__':
+    setup_package()
